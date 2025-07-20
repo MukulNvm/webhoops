3b6798e511c67e37eab86ee35655f6f36d55e17c
adityaglobe <129160391+adityaglobe@users.noreply.github.com>
2025-07-20 13:07:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Netflix Mockup</title>
+    <title>Netflix  Mockup</title>
     <style>
         body {
             margin: 0;
